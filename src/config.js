051488b367ae2f91552c1d93eb9cfb3ca32d3639
spetruzza <steve.petruzza@gmail.com>
2017-09-21 11:46:08 -0600

    fix update on palette change for 2d, remove toolbar for 2D

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // Settings for your webviewer installation
 
 // Server module URL
-DEFAULT_SERVER='http://molniya.sci.utah.edu:80/mod_visus?';
+DEFAULT_SERVER='https://feedback.llnl.gov/mod_visus?';
