c1c18b919873f0323d6d874c311530679b3ddf02
spetruzza <steve.petruzza@gmail.com>
2017-09-21 13:50:28 -0600

    add refresh button and autorefresh (default 5sec)

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // Settings for your webviewer installation
 
 // Server module URL
-DEFAULT_SERVER='https://feedback.llnl.gov/mod_visus?';
+DEFAULT_SERVER='http://cedmav7.sci.utah.edu:8083/mod_visus?'//https://feedback.llnl.gov/mod_visus?';
