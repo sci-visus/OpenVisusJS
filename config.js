d2a0d2ad05584924e15ae67d6410c36173ac148b
Cameron Christensen <cam@sci.utah.edu>
2017-09-25 15:33:29 -0600

    move examples to their own folder, all sources (except config) to source

---
+++ config.js
@@ -0,0 +1,5 @@
+// Settings for your webviewer installation
+
+// Server module URL
+DEFAULT_SERVER='http://cedmav7.sci.utah.edu:8083/mod_visus?';
+//DEFAULT_SERVER='https://feedback.llnl.gov/mod_visus?';
--- src/config.js
+++
@@ -1,4 +0,0 @@
-// Settings for your webviewer installation
-
-// Server module URL
-DEFAULT_SERVER='http://cedmav7.sci.utah.edu:8083/mod_visus?'//https://feedback.llnl.gov/mod_visus?';
