210175a467c40213eac79f979b613eef989b6d97
spetruzza <steve.petruzza@gmail.com>
2018-04-05 09:59:58 -0600

    reset default server

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 // Settings for your webviewer installation
 
 // Server module URL
-DEFAULT_SERVER='http://molniya.sci.utah.edu/mod_visus?';
-//DEFAULT_SERVER='http://cedmav7.sci.utah.edu:8083/mod_visus?';
-//DEFAULT_SERVER='https://feedback.llnl.gov/mod_visus?';
+DEFAULT_SERVER='http://cedmav7.sci.utah.edu:8083/mod_visus?';
