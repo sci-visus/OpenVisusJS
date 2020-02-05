19f4ff02bb68fb8e182cf96cf0f6589b27da5e14
Steven Callahan <steve@zero-epsilon.com>
2020-02-05 11:17:09 -0700

    Restrict server and dataset so that they are read-only. Stop enumerating datasets on server.

--- src/viewer.js
+++ src/viewer.js
@@ -173,33 +173,9 @@ function getServer() {
 async function setServer(value,pre_dataset=null)
 {
   document.getElementById('server').value=value;
-  
-  visusAsyncGetListOfDatasets(value+'&action=list&format=json')
-  .then(function (datasets) 
-  { 
-    dataset_widget=document.getElementById('dataset');
-    dataset_widget.options.length = 0;
-    for(var i = 0; i < datasets.length; i++) 
-    {
-      var value = datasets[i];
-      var item = document.createElement("option");
-      var n = value.indexOf("/");
-      //console.log("found / at position "+n+" for string "+ value)
-      // trimming prefix of the midx to first 3 characters(too long for the menu)
-      if(n > 0)
-        item.textContent = value.substring(0,Math.min(3,value.length))+value.substring(n,value.length)
-      else
-        item.textContent = value;
-      item.value = value;
-      dataset_widget.appendChild(item);
-    }     
     
-    if(pre_dataset==null)
-      setDataset(datasets[0]);
-    else
+  if (pre_dataset!=null)
       setDataset(pre_dataset, true)
-
-  });
 }
 
 
