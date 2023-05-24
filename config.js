2df529974c8020d5539bdbc3413ba70d251c0c61
Amy Gooch <amy.a.gooch@gmail.com>
2023-05-24 17:58:20 -0500

    remote server

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 
 // Server module URL
 
-//DEFAULT_SERVER='http://51.81.155.171:8081/mod_visus?';
-DEFAULT_SERVER='http://localhost:8080/mod_visus?';
+DEFAULT_SERVER='http://51.81.155.171:8081/mod_visus?';
+//DEFAULT_SERVER='http://localhost:8080/mod_visus?';
 
