a00c71e1e6d8e1b802f75b4e52bab57a241f6b6b
Amy Gooch <amy.a.gooch@gmail.com>
2023-06-29 15:53:44 -0500

    fix

--- config.js
+++ config.js
@@ -1,7 +1,14 @@
 // Settings for your webviewer installation
 
 // Server module URL
+const dotenv = require('dotenv');
+const path = require('path');
 
-//DEFAULT_SERVER='http://51.81.155.171:8081/mod_visus?';
-DEFAULT_SERVER='http://'+${DEPLOY_SERVER}':8080/mod_visus?';
+// Server module URL
+//DEFAULT_SERVER='http://10.1.10.57/mod_visus?';
+const envPath = path.join(__dirname, '../.env');
+dotenv.config({ path: envPath });
+const DEPLOY_SERVER = process.env.DEPLOY_SERVER || 'localhost';
 
+DEFAULT_SERVER='http://${DEPLOY_SERVER}:8080/mod_visus?';
+ 
