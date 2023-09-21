db2fc30fdb41d1c97025b599ae6e348fff590a64
Amy Gooch <amy.a.gooch@gmail.com>
2023-09-21 12:04:26 -0500

    putting back remote server

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Settings for your webviewer installation
 
-//DEFAULT_SERVER='http://51.81.155.171/mod_visus?';
-DEFAULT_SERVER='http://localhost:8080/mod_visus?';
+DEFAULT_SERVER='http://51.81.155.171/mod_visus?';
+//DEFAULT_SERVER='http://localhost:8080/mod_visus?';
 
