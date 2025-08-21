190fb5fbf86f4a1755e4fc5492bd03a4157d8174
Amy Gooch <amy.a.gooch@gmail.com>
2025-08-21 15:45:16 -0500

    no message

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 // Settings for your webviewer installation
 //remember to set
 //DEFAULT_SERVER='http://54.39.133.137/mod_visus?';
-DEFAULT_SERVER='http://'+process.env.DEPLOY_SERVER+'/mod_visus?';
+//DEFAULT_SERVER='http://'+process.env.DEPLOY_SERVER+'/mod_visus?';
+// Use the deploy_server variable that's set in the parent page
+DEFAULT_SERVER='http://'+deploy_server+'/mod_visus?';
